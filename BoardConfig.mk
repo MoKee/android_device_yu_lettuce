b9e9a14ef7ac949b34b354015ebf88c82b67827c
martincz <martincz@mokeedev.com>
2015-06-12 03:32:03 +0800

    mokee bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,9 +64,9 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_USES_UNCOMPRESSED_KERNEL := true
-TARGET_KERNEL_CONFIG := cyanogenmod_lettuce-64_defconfig
+TARGET_KERNEL_CONFIG := mokee_lettuce-64_defconfig
 else
-TARGET_KERNEL_CONFIG := cyanogenmod_lettuce_defconfig
+TARGET_KERNEL_CONFIG := mokee_lettuce_defconfig
 endif
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
 
@@ -90,8 +90,8 @@ BOARD_CAMERA_SENSORS := \
 TARGET_USE_VENDOR_CAMERA_EXT := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
-# CMHW
-BOARD_HARDWARE_CLASS := $(DEVICE_PATH)/cmhw/src
+# MKHW
+BOARD_HARDWARE_CLASS := $(DEVICE_PATH)/mkhw/src
 
 # malloc implementation
 MALLOC_IMPL := dlmalloc
