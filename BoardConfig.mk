a7ef9374b6ac360dbadc8ce1235bf2ab7c032065
Martincz Gao <martincz@mokeedev.com>
2018-09-13 12:12:10 +0800

    lettuce: mokee-ify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,10 +49,10 @@ BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
-TARGET_KERNEL_CONFIG := lineageos_lettuce_defconfig
+TARGET_KERNEL_CONFIG := mokee_lettuce_defconfig
 
-# LineageHW
-BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
+# MKHW
+BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/mkhw
 
 # Power
 TARGET_HAS_NO_POWER_STATS := true
